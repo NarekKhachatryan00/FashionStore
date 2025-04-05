15fae0348e5efbf71ca512e875badb195245e1da
Narek <narek@Noutbuk-Narek.local>
2025-04-05 19:50:51 +0400

    first commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,35 @@
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
+import './style/navbar.css';
+function Navbar() {
+    const [active, setActive] = useState('nav_menu');
+    const navToggle = () => {
+        active === 'nav_menu'
+            ? setActive('nav_menu nav_active') 
+            : setActive('nav_menu');
+    }
+
+    return (
+        <div>
+            <nav className="nav">
+                <h2>Alora</h2>
+                <div className="nav_input">
+                    <input type="search" placeholder="Search..."></input>
+                    <img src="./search.png"></img>
+                </div>
+                <ul className={active}>
+                    <li className="nav_item"><Link to="/">Home</Link></li>
+                    <li className="nav_item"><Link to="/">About</Link></li>
+                    <li className="nav_item"><Link to="/">Account</Link></li>
+                </ul>
+                <div onClick={navToggle} className="nav_toggler">
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
