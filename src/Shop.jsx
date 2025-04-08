479ebf9e253f231d82f22569a035194ddcefae03
Narek <narek@Noutbuk-Narek.local>
2025-04-08 21:21:47 +0400

    adding shop

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,36 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import './style/shop.css';
-import axios from 'axios';
 
 function Shop() {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const response = await axios.get('https://fakestoreapi.com/products');
-                setProducts(response.data);
-                setLoading(false);
-            } catch (error) {
-                console.error("Error fetching products:", error);
-                setLoading(false);
-            }
-        };
-
-        fetchProducts();
-    }, []);
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
     return (
         <div className="shop">
-            <div className="carousel">
-                {products.map((product) => (
-                    <div key={product.id} className="carousel-item">
-                        <img src={product.image} alt={product.title} />
-                    </div>
-                ))}
+            <div className="container">
+                <div className="content">
+                    <div className="content1"><h1>Content 1</h1></div>
+                    <div className="content2"><h1>Content 2</h1></div>
+                    <div className="content3"><h1>Content 3</h1></div>
+                    <div className="footer"><h1>Footer</h1></div>
+                </div>
             </div>
         </div>
     );
